02f76e051d7131e0d9e6f9db7f8e1ea09ce2e1ac
minhduc <minhduc1994vcu@gmail.com>
2019-06-15 17:37:15 +0700

    add request credential

---
+++ src/main/java/com/codegym/request/Credential.java
@@ -0,0 +1,25 @@
+package com.codegym.request;
+
+public class Credential {
+    private String user_name;
+    private String pass_word;
+
+    public Credential() {
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getPass_word() {
+        return pass_word;
+    }
+
+    public void setPass_word(String pass_word) {
+        this.pass_word = pass_word;
+    }
+}
